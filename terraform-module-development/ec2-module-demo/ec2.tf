1a971b34e1d6dddf309318a19b919c95de1434e6
Mohansai <bandarumohansai@gmail.com>
2024-09-23 22:33:22 +0530

    changes

--- terraform-module-development/ec2-module-demo/ec2.tf
+++ terraform-module-development/ec2-module-demo/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "git::https://github.com/Mohansai7-ctrl/terraform-practice/tree/main/terraform-module-development/terraform-aws-ec2.git"
+    source = "https://github.com/Mohansai7-ctrl/terraform-practice/tree/main/terraform-module-development/terraform-aws-ec2"
     #source = "https://github.com/Mohansai7-ctrl/terraform-practice/tree/main/terraform-module-development/terraform-aws-ec2"
     
     sg_id = ["sg-0d91387712ba38962"]
